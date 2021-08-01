63818b5d26e8a6ae06571a98f65552d4c5b7ff06
lym18610564123 <86158229+lym18610564123@users.noreply.github.com>
2021-08-01 14:24:27 +0800

    Update PSD.py

--- src/Features/PSD.py
+++ src/Features/PSD.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def power_spectral_density(signal, signal_frequency, window_size):
     """
-    Calculate the power spectral density on EEG signal, and then extract the band power of Delta, Theta, Alpha, Beta, and Gamma band.
+    Calculate the power spectral density of given signal.
     We support both 1d array input and DEEG standard input (p,m,e). For the later format, we calculate PSD on dim "e" and return a (p,m) numpy array.
     
     input:
@@ -11,13 +11,13 @@ def power_spectral_density(signal, signal_frequency, window_size):
         signal_frequency: int. Original frequency of the signal
         window_size: int. Size of the window 
     return
-        band power: tuple or numpy array, depends on the input shape
+        PSD: tuple or numpy array, depends on the input shape
     """
     def cal_psd(s, sf, ws):
         freqs, psd = scipy.signal.welch(s, sf, nperseg=ws)
         freq_res = freqs[1] - freqs[0]
 
-        delta = scipy.integrate.simps(psd[np.logical_and(freqs >= 0, freqs < 4)], dx = freq_res)
+        delta = scipy.integrate.simps(psd[:], dx = freq_res)
         theta = scipy.integrate.simps(psd[np.logical_and(freqs >= 4, freqs <= 7)], dx = freq_res)
         alpha = scipy.integrate.simps(psd[np.logical_and(freqs >= 8, freqs <= 15)], dx = freq_res)
         beta = scipy.integrate.simps(psd[np.logical_and(freqs >= 16, freqs <= 31)], dx = freq_res)
